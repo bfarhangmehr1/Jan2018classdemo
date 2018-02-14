988f5a170a1771c0467ef52f1fbfcaa231c562f7
bfarhangmehr1 <bfarhangmehr1@studentmail.nait.ca>
2018-02-14 14:25:44 -0700

    update the file

---
+++ Chinook.Data/DTOs/ArtistTitle.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region
+using Chinook.Data.POCOs;
+#endregion
+
+namespace Chinook.Data.DTOs
+{
+   public class ArtistTitle
+    {
+        public string title { get; set; }
+        public string artist { get; set; }
+        public List<TracksAndLength> songs { get; set; }
+    }
+}
---
+++ Chinook.Data/POCOs/TracksAndLength.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chinook.Data.POCOs
+{
+   public class TracksAndLength
+    {
+       public string songtitle { get; set; }
+        public string length { get; set; }
+    }
+}
